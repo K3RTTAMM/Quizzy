890e918e680d4dec0d1529f13f850f95ba795fac
Kert Tamm <31847373+K3RTTAMM@users.noreply.github.com>
2019-03-27 07:55:14 +0200

    Added quiz_page.dart and changed quiz.dart location

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+import '../UI/correct_wrong_overlay.dart';
+
+import './score_page.dart';
